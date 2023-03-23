cda7766ee3c0de0ff1da35d3187ef6551e411da7
dvolper <github@david.vollmers.org>
2023-03-23 16:10:49 +0100

    fixes

--- packages/Azure.Data.Tables.Poco/TypedTableClient.cs
+++ packages/Azure.Data.Tables.Poco/TypedTableClient.cs
@@ -100,7 +100,7 @@ public class TypedTableClient<T> where T : class
     {
         var asyncPageable = _tableClient.QueryAsync<TableEntity>(filter, maxPerPage, select, cancellationToken);
 
-        await foreach (var entity in asyncPageable)
+        await foreach (var entity in asyncPageable.WithCancellation(cancellationToken).ConfigureAwait(false))
         {
             var poco = _tableEntityConverter.ConvertFromEntity(entity);
 
