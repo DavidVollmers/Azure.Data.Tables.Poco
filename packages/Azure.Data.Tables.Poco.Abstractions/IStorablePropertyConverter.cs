9d91a06013a502c4c00fc17af82ec26c12ebfe64
dvolper <github@david.vollmers.org>
2023-03-23 12:44:02 +0100

    add missing abstractions

---
+++ packages/Azure.Data.Tables.Poco.Abstractions/IStorablePropertyConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace Azure.Data.Tables.Poco.Abstractions;
+
+public interface IStorablePropertyConverter
+{
+    bool IsKeyCompliant { get; }
+    
+    bool CanConvert(PropertyInfo propertyInfo);
+
+    object? ConvertTo(PropertyInfo propertyInfo, object instance);
+
+    object? ConvertFrom(PropertyInfo propertyInfo, object? value);
+}
