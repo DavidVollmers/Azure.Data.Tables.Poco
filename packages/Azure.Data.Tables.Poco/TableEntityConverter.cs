ebd944a4701f06d289d8414cbd0b19bd9ca638b4
dvolper <github@david.vollmers.org>
2024-12-14 21:34:22 +0100

    make TableEntityConverter usage consistent & easier

--- packages/Azure.Data.Tables.Poco/TableEntityConverter.cs
+++ packages/Azure.Data.Tables.Poco/TableEntityConverter.cs
@@ -4,19 +4,19 @@ namespace Azure.Data.Tables.Poco;
 
 public sealed class TableEntityConverter<T> where T : class
 {
-    private readonly PocoSchema _schema = PocoSchema.CreateFromType(typeof(T));
+    public PocoSchema Schema { get; } = PocoSchema.CreateFromType(typeof(T));
 
-    public ITableEntity ConvertToEntity(T poco, bool keysOnly = false)
+    public TableEntity ConvertToEntity(T poco, bool keysOnly = false)
     {
         var entity = new TableEntity
         {
-            PartitionKey = (string)_schema.PartitionKey.GetValue(poco)!,
-            RowKey = (string)_schema.RowKey.GetValue(poco)!
+            PartitionKey = (string)Schema.PartitionKey.GetValue(poco)!,
+            RowKey = (string)Schema.RowKey.GetValue(poco)!
         };
 
         if (keysOnly) return entity;
 
-        foreach (var property in _schema.GetProperties)
+        foreach (var property in Schema.GetProperties)
         {
             if (property.ShouldBeIgnored) continue;
 
@@ -30,9 +30,9 @@ public sealed class TableEntityConverter<T> where T : class
 
     public T ConvertFromEntity(TableEntity entity)
     {
-        var poco = (T)_schema.Constructor.CreateInstance(entity);
+        var poco = (T)Schema.Constructor.CreateInstance(entity);
 
-        foreach (var property in _schema.SetProperties)
+        foreach (var property in Schema.SetProperties)
         {
             if (property is { ShouldBeIgnored: true, IsPartitionKey: false, IsRowKey: false }) continue;
 
